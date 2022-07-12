e62abe62ab2d26b8c5187c28ebc4d4d9b79deb02
vg1009 <72339951+vg1009@users.noreply.github.com>
2022-07-12 16:55:59 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,18 +3,18 @@ showNotes();
 
 // If user adds a note, add it to the localStorage
 let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener('click', function (e) {
+addBtn.addEventListener('click', function (e) { //e=eventListener
     let addTxt = document.getElementById("addTxt");
-    let notes = localStorage.getItem("notes");
+    let notes = localStorage.getItem("notes");//it will return items named with notes from local storage
     if (notes == null) {
         notesObj = [];
     }
     else {
         notesObj = JSON.parse(notes);
     }
-    notesObj.push(addTxt.value);
-    localStorage.setItem('notes', JSON.stringify(notesObj));
-    addTxt.value = "";
+    notesObj.push(addTxt.value); //update local storage
+    localStorage.setItem('notes', JSON.stringify(notesObj)); //convert notesObj into string as localStorage only stores strings
+    addTxt.value = ""; //after adding note set addTxt blank 
     console.log(notesObj);
     showNotes();
 })
@@ -36,7 +36,7 @@ function showNotes() {
                     <p class="card-text"> ${element}</p>
                     <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                 </div>
-            </div>`;
+            </div>`; //this will append html string in html card
     });
     let notesElm = document.getElementById('notes');
     if (notesObj.length != 0) {
@@ -58,16 +58,19 @@ function deleteNote(index){
         notesObj = JSON.parse(notes);
     }
 
+    //splice takes two args starting index and no of items you want to remove
     notesObj.splice(index,1);
-    localStorage.setItem('notes', JSON.stringify(notesObj));
+    localStorage.setItem('notes', JSON.stringify(notesObj)); //update localstorage
     showNotes();
 }
+
+//searchTxt is an id in html code within nav bar section
 search = document.getElementById('searchTxt');
 search.addEventListener("input",function(){
     let inputVal = search.value.toLowerCase();
     console.log('Input event Fired!',inputVal);
     let noteCards = document.getElementsByClassName('noteCard');
-    Array.from(noteCards).forEach(function(element){
+    Array.from(noteCards).forEach(function(element){ //for each noteCards store its paragraph content in cardTxt
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
         if(cardTxt.includes(inputVal)){
             element.style.display = "block";
